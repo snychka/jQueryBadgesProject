0c3e8129dac7d9a8700e842a4c5dc162499f0d53
snychka <stefannychka@gmail.com>
2017-06-01 11:25:36 -0600

    try slightly different solution from repo.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
+    $.ajax({
+      url: 'https://www.codeschool.com/users/lraulin.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+        console.log(JSON.stringify(response.courses.completed));
+        for (var i = 0; i < response.courses.completed.length; i++) {
+          var title = response.courses.completed[i].title;
+          var url = response.courses.completed[i].url;
+          var badge = response.courses.completed[i].badge;
+          $("#badges").append('<div class="course"><h3>' + title + '</h3><img src="' + badge + '"><a href="' + url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+        }
+      }
+    });
 
+  /*
   $.ajax({
       url: 'https://www.codeschool.com/users/lraulin.json',
       dataType: 'jsonp',
@@ -12,6 +27,7 @@ $(function() {
         }
       }
     });
+  */
 
   /*
   $.ajax({
