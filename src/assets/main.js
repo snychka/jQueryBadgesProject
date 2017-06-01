c3ee5637377e5c37733ae103917dfdd26de68e04
snychka <stefannychka@gmail.com>
2017-06-01 11:20:45 -0600

    try alternative solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,27 @@
 $(function() {
 
   $.ajax({
+      url: 'https://www.codeschool.com/users/lraulin.json',
+      dataType: 'jsonp',
+      success: function(response) {
+          for (var i = 0; i < response.courses.completed.length; i++) {
+          var title = response.courses.completed[i].title;
+          var url = response.courses.completed[i].url;
+          var badge = response.courses.completed[i].badge;
+          $("#badges").append('<div class="course"><h3>' + title + '</h3><img src="' + badge + '"><a href="' + url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+        }
+      }
+    });
+
+  /*
+  $.ajax({
     url: 'https://www.codeschool.com/users/sergiocruz.json',
     dataType: 'jsonp',
     success: function(data) {
       populateWithCourses(data.courses.completed)
     }
   });
+  */
 
   function populateWithCourses(courses) {
 
